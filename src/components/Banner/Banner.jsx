d62281a11088caa653fb8f097f6b0acb79cddfd3
muhammad-shahin <muhammadshahin002@gmail.com>
2023-12-22 11:23:04 +0600

    banner content  updated

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,6 +2,7 @@ import PrimaryButton from '../../shared/PrimaryButton/PrimaryButton';
 import Lottie from 'lottie-react';
 import bannerAnim from '../../assets/Animation/task-anim.json';
 import ParticlesBackground from '../ParticlesBackground/ParticlesBackground';
+import { Link } from 'react-router-dom';
 const Banner = () => {
   return (
     <section className='xl:px-[8%] xl:py-20 px-[5%] py-10 backdrop-blur-xl bg-opacity-[0.75] flex-bet relative gradient-bg'>
@@ -13,16 +14,17 @@ const Banner = () => {
       {/* left side */}
       <div className='capitalize text-white space-y-6 relative'>
         <h1 className='font-semibold text-2xl w-full lg:text-6xl lg:max-w-3xl'>
-          Unlock Your Academic Potential: Transform Your Study Experience with
-          Collaborative Learning
+          DnD Task Management: Your Gateway to Effortless Productivity, Task
+          Mastery, and Collaborative Success
         </h1>
         <p className='font-semibold lg:text-base text-sm lg:max-w-3xl text-slate-600'>
-          Join our online group study community and experience a new way to
-          excel in your studies. Collaborate with friends, create assignments,
-          and achieve academic success, all in one place.
+          Unleash the full potential of your productivity as you explore the
+          intuitive features of DnD Task Management. From drag-and-drop task
+          management to real-time collaboration, our platform is tailored to
+          enhance every aspect of your workflow.
         </p>
         <div className='flexme'>
-          <PrimaryButton text="Let's Explore" />
+          <Link to='/login'><PrimaryButton text="Let's Explore" /></Link>
         </div>
       </div>
 
