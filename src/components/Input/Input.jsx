1b93cc79da1fc1005f39da49f1addf59a6e04e56
muhammad-shahin <muhammadshahin002@gmail.com>
2023-12-22 11:28:31 +0600

    input button show password toggle style change

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -97,32 +97,34 @@ const Input = ({
           <span className='text-red-600 text-[22px] font-medium'>*</span>
         )}{' '}
       </label>
-      <input
-        className='w-[100%] border-2 border-gray px-5 py-2 text-[18px] font-medium text-[#000] placeholder:text-gray placeholder:text-[16px] rounded outline-2 outline-secondary'
-        type={showPassword ? 'text' : type}
-        name={name}
-        placeholder={placeholder}
-        onChange={onChange}
-        onBlur={onBlur}
-        required={isRequired}
-        id={name}
-        readOnly={readOnly}
-        defaultValue={defaultValue}
-      />
-      {type === 'password' && (
-        <AiFillEye
-          title='Click To Show or Hide Password'
-          onClick={() => {
-            setShowPassword(!showPassword);
-          }}
-          className={`absolute top-[40px] md:top-[35px] md:right-[20px] right-[10px] text-[24px] md:text-[32px] ${
-            showPassword ? 'text-sky-500' : 'text-gray-700'
-          } cursor-pointer`}
+      <div className='relative'>
+        <input
+          className='w-[100%] border-2 border-gray px-5 py-2 text-[18px] font-medium text-[#000] placeholder:text-gray placeholder:text-[16px] rounded outline-2 outline-secondary'
+          type={showPassword ? 'text' : type}
+          name={name}
+          placeholder={placeholder}
+          onChange={onChange}
+          onBlur={onBlur}
+          required={isRequired}
+          id={name}
+          readOnly={readOnly}
+          defaultValue={defaultValue}
         />
-      )}
-      {errorMessage && (
-        <p className='text-[14px] text-red-500'>{errorMessage}</p>
-      )}
+        {type === 'password' && (
+          <AiFillEye
+            title='Click To Show or Hide Password'
+            onClick={() => {
+              setShowPassword(!showPassword);
+            }}
+            className={`absolute top-[50%] translate-y-[-50%] right-[10px] text-[24px] md:text-[32px] ${
+              showPassword ? 'text-sky-500' : 'text-[#888181]'
+            } cursor-pointer`}
+          />
+        )}
+        {errorMessage && (
+          <p className='text-[14px] text-red-500'>{errorMessage}</p>
+        )}
+      </div>
     </div>
   );
 };
